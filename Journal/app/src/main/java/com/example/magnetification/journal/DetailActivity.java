6b471d42ff0d311b3f6cfa21959d14059752344f
Marcelvla <marcelvdla@gmail.com>
2018-11-29 00:16:23 +0100

    working detalactivity

--- Journal/app/src/main/java/com/example/magnetification/journal/DetailActivity.java
+++ Journal/app/src/main/java/com/example/magnetification/journal/DetailActivity.java
@@ -1,7 +1,13 @@
 package com.example.magnetification.journal;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +15,58 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+
+        String title = (String) intent.getSerializableExtra("title");
+        String text = (String) intent.getSerializableExtra("text");
+        int mood = (int) intent.getSerializableExtra("mood");
+        String time = (String) intent.getSerializableExtra("time");
+
+        ImageView moodView = findViewById(R.id.moodImage);
+        switchMood(mood, moodView);
+
+        TextView titleView = findViewById(R.id.entryTitle);
+        titleView.setText(title);
+
+        TextView contentView = findViewById(R.id.journalEntry);
+        contentView.setText(text);
+
+        TextView timestamp = findViewById(R.id.timeStamp);
+        timestamp.setText(time);
+
+    }
+
+    public void switchMood(int moodid, View v) {
+        ImageView moodIm = v.findViewById(R.id.moodImage);
+        switch (moodid) {
+            case 1:
+                moodIm.setImageResource(R.drawable.afraid);
+                break;
+            case 2:
+                moodIm.setImageResource(R.drawable.angry);
+                break;
+            case 3:
+                moodIm.setImageResource(R.drawable.embarrased);
+                break;
+            case 4:
+                moodIm.setImageResource(R.drawable.happy);
+                break;
+            case 5:
+                moodIm.setImageResource(R.drawable.joyous);
+                break;
+            case 6:
+                moodIm.setImageResource(R.drawable.proud);
+                break;
+            case 7:
+                moodIm.setImageResource(R.drawable.sad);
+                break;
+            case 8:
+                moodIm.setImageResource(R.drawable.upset);
+                break;
+            case 9:
+                moodIm.setImageResource(R.drawable.worried);
+                break;
+        }
     }
 }
--- Journal/app/src/main/java/com/example/magnetification/journal/MainActivity.java
+++ Journal/app/src/main/java/com/example/magnetification/journal/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.magnetification.journal;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -30,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
         EntryAdapter adapter = new EntryAdapter(this, cs);
         ListView list = findViewById(R.id.entryList);
         list.setAdapter(adapter);
-//        list.setOnItemClickListener(new ListItemClickListener());
+        list.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Cursor clickedEntry = (Cursor) adapterView.getItemAtPosition(i);
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            intent.putExtra("title", clickedEntry.getString(1));
+            intent.putExtra("text", clickedEntry.getString(2));
+            intent.putExtra("mood", clickedEntry.getInt(3));
+            intent.putExtra("time", clickedEntry.getString(4));
+            startActivity(intent);
+        }
     }
 }
